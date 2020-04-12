220fb13d7da36c2ce941b22a7bb5a4270e58a2fe
marius2393 <marius.giebenhain@web.de>
2020-04-12 15:21:17 +0200

    basic structures and functions

---
+++ code/dInw_functions_multi.jl
@@ -0,0 +1,361 @@
+using DelimitedFiles
+using Random
+using Statistics
+
+#cd("C:/Users/Marius/Documents/INW/DecisionInfluenceNetwork/code")
+#include("dInw_structures_multi.jl")
+
+
+function train!(
+        dInw::DecisionInfluenceNetwork;
+        p::_param = _param(),
+        g_init::Array{Float32, 2} = Array{Float32, 2}(undef, 0, 0),
+        g_in::Array{Float32, 3} = Array{Float32, 3}(undef, 0, 0, 0),
+        g_out::Array{Float32, 2} = Array{Float32, 2}(undef, 0, 0),
+        mom_init::Array{Float32, 2} = Array{Float32, 2}(undef, 0, 0),
+        mom_in::Array{Float32, 3} = Array{Float32, 3}(undef, 0, 0, 0),
+        mom_out::Array{Float32, 2} = Array{Float32, 2}(undef, 0, 0))::Nothing
+    trainer::_trainer = _trainer(dInw.inw, g_init = g_init, g_in = g_in, g_out = g_out, mom_init = mom_init, mom_in = mom_in, mom_out = mom_out)
+    internal::_internal = _internal(dInw.inw)
+    predictor::_predictor = _predictor(dInw.inw, dInw.data)
+    n::Int = dInw.data.n
+    mse::Float32 = 1f0
+    mse_::Float32 = 0f0
+    batchIds::Array{Int, 1} = p.batchSize == -1 ? 
+        [1, n-p.testSize+1] : vcat(collect(1:p.batchSize:(n-p.testSize-1)), [n-p.testSize+1])
+    batches::Array{Int, 1} = shuffle(p.testSize+1:n)
+    for i in 1:p.maxIter
+        for j in 1:length(batchIds)-1
+            print(string(j))
+            _train!(
+                dInw.inw, 
+                dInw.data.X[batches[batchIds[j]:batchIds[j+1]-1],:], 
+                dInw.data.Y[batches[batchIds[j]:batchIds[j+1]-1],:], 
+                trainer, 
+                internal, 
+                p)
+        end
+        batches .= shuffle(batches)
+        if i%p.feedback == 0 
+            predict!(
+                predictor,
+                dInw.data,
+                dInw.inw)
+            if p.testSize != 0
+                mse_ = sum((dInw.data.Y[1:p.testSize,:] .- predictor.out[1:p.testSize,:]) .^ 2, dims = (1,2))[1]/(p.testSize*dInw.inw.m)
+                print(string(
+                    "Finished Iteration ", string(i), 
+                    "\nMSE Training Data: ", string(sum((dInw.data.Y[p.testSize+1:end,:] .- predictor.out[p.testSize+1:end,:]) .^ 2, dims = (1,2))[1]/((n-p.testSize)*dInw.inw.m)), 
+                    "\nMSE Test Data:     ", string(mse_),
+                    "\n\n"))
+                if ((mse - mse_) > p.thresh) | (i <= p.minIter)
+                    mse = mse_
+                else 
+                    break 
+                end
+            else
+                print(string(
+                    "Finished Iteration ", string(i), "\nMSE: ", sum((dInw.data.Y .- predictor.out) .^ 2, dims = (1,2))[1]/n,  "\n\n"))
+            end
+        end
+    end
+    return nothing
+end
+
+
+function cross_train!(
+        dInw::DecisionInfluenceNetwork,
+        k::Int;
+        z_crit::Float32 = -1f0,
+        p::_param = _param())::Nothing
+    n::Int = dInw.data.n
+    m::Int = dInw.inw.n_
+    subset_::Array{Bool, 1} = Array{Bool, 1}(undef, n)
+    trainer::_trainer = _trainer(dInw.inw)
+    internal::_internal = _internal(dInw.inw)
+    predictor::_predictor = _predictor(dInw.inw, dInw.data)
+    w_init_::Array{Float32, 2} = Array{Float32, 2}(undef, m, k) 
+    w_in_::Array{Float32, 3} = Array{Float32, 3}(undef, m, m, k) 
+    w_out_::Array{Float32, 2} = Array{Float32, 2}(undef, m, k) 
+    sub_ids::Array{Int, 1} = Int.(floor.([i for i in 0:(n/k):n])).+1
+    for i in 1:k
+        print(string(
+            "Starting Round ", string(i), "\n\n"))
+        subset_ .= [.!(j in sub_ids[i]:sub_ids[i+1]-1) for j in 1:n]
+        n_sub::Int = sum(subset_)
+        mse::Float32 = 1.0f0
+        mse_::Float32 = 1.0f0
+        for j in 1:p.maxIter
+            _train!(
+                dInw.inw,
+                dInw.data.X[subset_,:],
+                dInw.data.Y[subset_],
+                trainer,
+                internal,
+                p)
+            if j%p.feedback == 0 
+                predict!(
+                    predictor,
+                    dInw.data,
+                    dInw.inw)
+                mse_ = sum((dInw.data.Y[.!subset_] .- predictor.out[.!subset_]) .^ 2)/(n-n_sub)
+                print(string(
+                    "Finished Iteration ", string(j), 
+                    "\nMSE Training Data: ", string(sum((dInw.data.Y[subset_] .- predictor.out[subset_]) .^ 2)/n_sub), 
+                    "\nMSE Test Data:     ", string(mse_),
+                    "\n\n"))
+                if ((mse - mse_) > p.thresh) | (j <= p.minIter)
+                    w_init_[:,i] .= dInw.inw.w_init
+                    w_in_[:,:,i] .= dInw.inw.w_in
+                    w_out_[:,i] .= dInw.inw.w_out
+                    mse = mse_
+                else
+                    break
+                end
+            end
+        end
+        dInw.inw.w_init .= (rand(Float32, m) .- 0.5f0) .* dInw.inw.scale 
+        dInw.inw.w_in .= (rand(Float32, m, m) .- 0.5f0) .* dInw.inw.free .* dInw.inw.scale
+        dInw.inw.w_out .= (rand(Float32, m) .- 0.5f0) .* dInw.inw.scale 
+        trainer.mom_init .= 0.0f0
+        trainer.mom_in .= 0.0f0
+        trainer.mom_out .= 0.0f0
+    end
+    dInw.inw.w_init .= mean(w_init_, dims = 2)[:]
+    dInw.inw.w_in .= mean(w_in_, dims = 3)[:,:]
+    dInw.inw.w_out .= mean(w_out_, dims = 2)[:]
+    if z_crit != -1f0
+        dInw.inw.free .= Float32.(abs.(dInw.inw.w_in) .>= std(w_in_, mean = dInw.inw.w_in, dims = 3)[:,:] .* z_crit)
+        dInw.inw.w_in .*= dInw.inw.free
+    end
+    return nothing
+end
+
+
+function _train!(
+        inw::InfluenceNetwork,
+        X::Array{Int, 2},
+        Y::Array{Float32, 2},
+        trainer::_trainer,
+        internal::_internal,
+        p::_param)::Nothing
+    f_::Float32 = p.batchSize .== -1 ? 1 : size(X)[1]/p.batchSize
+    trainer.g_init .= 0f0
+    trainer.g_in .= 0f0
+    trainer.g_out .= 0f0
+    trainer.upd_count_in .= 1f0
+    trainer.upd_count_out .= 1f0
+    for i in 1:size(Y)[1]
+        # compute local gradients
+        for j in 1:size(Y)[2]
+            set_internal!(internal, inw, X[i,:], j)
+            forward_prop!(internal, inw)
+            internal.g_init[:,j] .= internal.g_init_'*internal.w_out
+            internal.g_in[:,j] .= internal.g_in_'*internal.w_out 
+            internal.g_out[:,j] .= internal.s
+            internal.out[j,:] .= (internal.w_out' * internal.s) * inw.beta
+        end
+        back_prop!(internal, Y[i,:], inw)
+        # update global gradients
+        trainer.g_init[internal.select, :] .+= internal.g_init
+        trainer.g_in[internal.select, internal.select, :] .+= reshape(internal.g_in, inw.n, inw.n, inw.m)
+        trainer.g_in .*= inw.free
+        trainer.g_out[internal.select, :] .+= internal.g_out  
+        trainer.upd_count_in[internal.select, internal.select] .+= 1f0
+        trainer.upd_count_out[internal.select] .+= 1f0
+    end
+    #trainer.update_in .= trainer.upd_count_in .> 1f0
+    #trainer.update_out .= trainer.upd_count_out .> 1f0
+    trainer.g_init ./= trainer.upd_count_out 
+    trainer.g_in ./= trainer.upd_count_in
+    trainer.g_out ./= trainer.upd_count_out
+    trainer.mom_init .= (trainer.mom_init .+ trainer.g_init) .* p.mom # [trainer.update_out, :]
+    trainer.mom_in .= (trainer.mom_in .+ trainer.g_in) .* p.mom # [trainer.update_in, :]
+    trainer.mom_out .= (trainer.mom_out .+ trainer.g_out) .* p.mom # [trainer.update_out, :]
+    inw.w_init .+= f_ * p.lr * p.lr_fact .* (trainer.g_init .+ trainer.mom_init)
+    inw.w_in .+= f_ * p.lr * p.lr_fact .* (trainer.g_in .+ trainer.mom_in)
+    inw.w_out .+= f_ * p.lr .* (trainer.g_out .+ trainer.mom_out)
+    return nothing
+end
+
+
+function set_internal!(
+        internal::_internal,
+        inw::InfluenceNetwork, 
+        x::Array{Int, 1},
+        j::Int)::Nothing
+    internal.select .= x.+inw.index
+    internal.s .= 1f0
+    internal.w_init .= inw.w_init[internal.select, j]
+    internal.w_in .= inw.w_in[internal.select, internal.select, j]
+    internal.w_out .= inw.w_out[internal.select, j]
+    internal.g_init_ .= 0f0
+    internal.g_in_ .= 0f0
+    return nothing
+end
+
+
+function forward_prop!(
+        internal::_internal,
+        inw::InfluenceNetwork)::Nothing
+    n = inw.n
+    internal.z .= internal.w_init .* internal.s
+    internal.expZ .= _expZ.(internal.z)
+    internal.s .= _logit.(internal.z, internal.expZ)
+    internal.g_init_[1:(n+1):end] .= 2f0 .* internal.s .* (1f0.-internal.s)
+    internal.s .= internal.s .* 2f0 .- 1f0
+    for t in 1:inw.rounds
+        internal.s[n] = inw.bias_in
+        internal.z .= internal.w_in' * internal.s                        # calculate update scores
+        internal.expZ .= _expZ.(internal.z)
+        internal.dnom .= _dnom.(internal.s, internal.z, internal.expZ, inw.c)      # calculate denominater function values
+        internal.num .= _num.(internal.s, internal.z, internal.expZ, inw.c)
+        # Update w_init gradient
+        internal.t_1 .= init_t_1_.(internal.z, internal.expZ, internal.num, internal.dnom, inw.c)
+        internal.t_2 .= init_t_2_.(internal.s, internal.z, internal.expZ, internal.num, internal.dnom, inw.c)
+        internal.w_in[1:(n+1):end] .= internal.t_1 ./ internal.t_2
+        internal.g_init_ .= internal.w_in' * internal.g_init_
+        internal.g_init_ .*= internal.t_2 ./ internal.dnom.^2
+        # Update w_in gradient
+        internal.t_1 .= (internal.s .^ 2f0 .- 1f0)                    # temp variable
+        internal.w_in[1:(n+1):end] .= -2f0 ./ (internal.t_1 .+ inw.c)      # modify weight matrix for on the go updating of d_in
+        internal.g_in_ .= internal.w_in' * internal.g_in_                  # initialize with weighted sum of prev derivatives
+        _addprev!(internal.g_in_, internal.s)
+        internal.g_in_ .*= -2f0 .* internal.t_1 .* internal.expZ  # part 2 of on the go updating
+        internal.g_in_ ./= internal.dnom .^ 2                        # final derivative of current stage
+        # calculate new scores and reset weights
+        internal.s .= internal.num ./ internal.dnom
+        internal.w_in[1:(n+1):end] .= 0.0f0
+    end
+    internal.s[end] = inw.bias_out
+    return nothing
+end
+
+
+
+function back_prop!(
+        internal::_internal,
+        y::Array{Float32,1},
+        inw::InfluenceNetwork)::Nothing
+    max_::Float32 = maximum(internal.out)
+    internal.out .= exp.(internal.out .- max_)
+    sum_ = sum(internal.out)
+    internal.out ./= sum_
+    internal.g_loss_ .= reshape(kron(internal.out, internal.out), inw.m, inw.m)
+    internal.g_loss_[1:inw.m+1:inw.m^2] .= (internal.out) .* (internal.out .- 1f0)#(1f0 .- internal.out)
+    internal.g_loss_ .*= (internal.out .- y)
+    internal.g_loss .= sum(internal.g_loss_, dims = 1)[:]
+    internal.g_loss .*= 2f0*inw.beta
+    internal.g_init' .*= internal.g_loss
+    internal.g_in' .*= internal.g_loss
+    internal.g_out' .*= internal.g_loss
+    return nothing
+end
+
+
+
+function predict(
+        dInw::DecisionInfluenceNetwork;
+        X::Array{Int, 2} = Array{Int, 2}(undef, 0, 0),
+        Y::Array{Int, 1} = Array{Int, 1}(undef, 0))::_predictor
+    if size(X)[1] == 0
+        data = dInw.data
+    else    
+        data = _data(X, Y)
+    end
+    predictor = _predictor(dInw.inw, data)
+    predict!(predictor, data, dInw.inw)
+    return predictor
+end
+
+function predict!(
+        predictor::_predictor, 
+        data::_data, 
+        inw::InfluenceNetwork)::Nothing
+    predictor.S .= 0f0
+    predictor.Z .= 0f0
+    for i in 1:data.n
+        predictor.select .= data.X[i,:].+inw.index
+        predictor.Z .= inw.w_init[predictor.select, :]
+        predictor.S .= _logit.(predictor.Z, _expZ.(predictor.Z)).*2f0.-1f0
+        for t in 1:inw.rounds
+            predictor.S[inw.n,:] .= inw.bias_in
+            for k in 1:inw.m 
+                predictor.Z[:,k] .= inw.w_in[predictor.select, predictor.select, k]' * predictor.S[:,k] 
+            end
+            predictor.S .= 
+                _num.(predictor.S, predictor.Z, _expZ.(predictor.Z), inw.c)./
+                _dnom.(predictor.S, predictor.Z, _expZ.(predictor.Z), inw.c)
+        end
+        predictor.S[inw.n,:] .= inw.bias_out
+        predictor.out[i,:] .= diag(inw.w_out[predictor.select, :]' * predictor.S)
+        max_::Float32 = maximum(predictor.out[i,:])
+        predictor.out[i,:] .= exp.(predictor.out[i,:] .- max_)
+        sum_::Float32 = sum(predictor.out[i,:])
+        predictor.out[i,:] ./= sum_
+    end
+    return nothing
+end
+
+
+function _expZ(z::Float32)::Float32
+    if z > 0f0
+        return exp(-z)
+    else
+        return exp(z)
+    end
+end
+
+
+function _dnom(x::Float32, z::Float32, expZ::Float32, c::Float32)::Float32 
+    if z > 0f0
+        return (1f0+x)+(1f0-x)*expZ+c
+    else
+        return (1f0+x)*expZ+(1f0-x)+c
+    end
+end
+    
+function _num(x::Float32, z::Float32, expZ::Float32, c::Float32)::Float32 
+    if(z > 0f0)
+        return (1f0+x)-(1f0-x)*expZ+c
+    else
+        return (1f0+x)*expZ-(1f0-x)+c
+    end
+end
+    
+function init_t_1_(z::Float32, expZ::Float32, num::Float32, dnom::Float32, c::Float32)::Float32
+    if z > 0f0
+        return (1f0+expZ)*dnom-(1f0-expZ)*num
+    else
+        return (1f0+expZ)*dnom+(1f0-expZ)*num
+    end
+end
+
+function init_t_2_(x::Float32, z::Float32, expZ::Float32, num::Float32, dnom::Float32, c::Float32)::Float32
+    if z > 0f0
+        return (1f0-x)*expZ*(dnom-num)+c
+    else
+        return (1f0+x)*expZ*(dnom-num)+c
+    end
+end
+
+
+function _logit(z::Float32, expZ::Float32)::Float32
+    if z > 0.0f0
+        return 1.0f0/(1.0f0+expZ)
+    else
+        return expZ/(1.0f0+expZ)
+    end
+end
+
+
+function _addprev!(d_in::Array{Float32,2}, x::Array{Float32,1})::Nothing # updates derivative matrix by adding previous activation scores for some entries... check the math in technical report
+    n = size(d_in)[1]
+    d_in[collect(Iterators.flatten([i:n^2+1:n^3 for i in 1:n:n^2]))] .+= repeat(x, inner = n)
+    return nothing
+end  
+
+
+
+
+
---
+++ code/dInw_structures_multi.jl
@@ -0,0 +1,274 @@
+using LinearAlgebra
+
+
+struct InfluenceNetwork 
+    n::Int
+    n_::Int
+    m::Int
+    rounds::Int
+    bias_in::Float32
+    bias_out::Float32
+    beta::Float32
+    c::Float32
+    scale::Float32
+    index::Array{Int, 1}
+    w_init::Array{Float32, 2}
+    w_in::Array{Float32, 3}
+    w_out::Array{Float32, 2}
+    free::Array{Float32, 3}
+    function InfluenceNetwork(
+            n::Int,
+            m::Int;
+            rounds::Int = 4,
+            bias_in::Bool = true,
+            bias_out::Bool = true,
+            beta::Float32 = 1f0,
+            c::Float32 = 1f-9,
+            scale::Float32 = 1f-2,
+            index::Array{Int, 1} = Array{Int, 1}(undef, 0),
+            w_init::Array{Float32, 2} = Array{Float32, 2}(undef, 0, 0),
+            w_in::Array{Float32, 3} = Array{Float32, 3}(undef, 0, 0, 0),
+            w_out::Array{Float32, 2} = Array{Float32, 2}(undef, 0, 0),
+            free::Array{Float32, 3} = Array{Float32, 3}(undef, 0, 0, 0))
+        scale *= 2.0f0
+        if size(index)[1] == 0
+            index = collect(1:2:2*n)
+        end
+        n_::Int = index[end]
+        if size(free)[1] != n_
+            free = reshape(repeat(Float32.(.!(Array{Bool, 2}(I, n_, n_))), m)', n_, n_, m)
+            free[:,n_,:] .= 0f0
+            #free = Float32.(.!(Array{Bool, 2}(I, n_, n_, m)))
+            #free[:,size(free)[1]] .= 0.0f0
+        end
+        if size(w_init)[1] != n_
+            w_init = (rand(Float32, n_, m) .- 0.5f0) .* scale 
+        end
+        if size(w_in)[1] != n_
+            w_in = (rand(Float32, n_, n_, m) .- 0.5f0) .* free .* scale
+        end
+        if size(w_out)[1] != n_
+            w_out = (rand(Float32, n_, m) .- 0.5f0) .* scale 
+        end
+        return new(
+            n,
+            n_,
+            m,
+            rounds,
+            Float32(bias_in),
+            Float32(bias_out),
+            beta,
+            c,
+            scale,
+            index,
+            w_init,
+            w_in,
+            w_out,
+            free)
+    end
+end
+
+
+struct _data
+    X::Array{Int, 2}
+    Y::Array{Float32, 2}
+    n::Int
+    m::Int
+    function _data(
+            X::Array{Int, 2}, 
+            Y::Array{Int, 1};
+            binary::Bool = false,
+            shuffle_::Bool = false)
+        n::Int = size(X)[1]
+        m::Int = size(X)[2]
+        nCases::Int = binary ? 1 : length(unique(Y))
+        order::Array{Int, 1} = shuffle_ ? shuffle(1:n) : collect(1:n)
+        X_::Array{Int, 2} = zeros(Int, (n, m+1))
+        X_[1:n,1:m] .= X[order,:]
+        if length(Y) > 0
+            Y_ = zeros(Float32, n, nCases)
+            for i in 1:n
+                Y_[i,Y[order[i]]+1] = 1f0
+            end
+        else
+            Y_ = Array{Float32, 2}(undef,0,0)
+        end
+        return new(X_, Y_, n, m)
+    end
+end
+
+
+struct DecisionInfluenceNetwork
+    inw::InfluenceNetwork
+    data::_data
+    function DecisionInfluenceNetwork(
+            X::Array{Int, 2}, 
+            Y::Array{Int, 1};
+            binary::Bool = false,
+            shuffle_::Bool = false,
+            rounds::Int = 4,
+            bias_in::Bool = true,
+            bias_out::Bool = true,
+            c::Float32 = 1f-9,
+            scale::Float32 = 1f-2,
+            w_init::Array{Float32, 2} = Array{Float32, 2}(undef, 0, 0),
+            w_in::Array{Float32, 3} = Array{Float32, 3}(undef, 0, 0, 0),
+            w_out::Array{Float32, 2} = Array{Float32, 2}(undef, 0, 0),
+            free::Array{Float32, 3} = Array{Float32, 3}(undef, 0, 0, 0))
+        data = _data(
+            X,
+            Y,
+            binary = binary,
+            shuffle_ = shuffle_)
+        index::Array{Int, 1} = ones(Int, data.m+1)
+        for i in 1:data.m
+            index[i+1] = index[i] + length(unique(data.X[:,i]))
+        end
+        inw = InfluenceNetwork(
+            data.m+1,
+            size(data.Y)[2],
+            rounds = rounds,
+            bias_in = bias_in,
+            bias_out = bias_out,
+            c = c,
+            scale = scale,
+            index = index,
+            w_init = w_init,
+            w_in = w_in,
+            w_out = w_out,
+            free = free)
+        return new(inw, data)
+    end
+end
+
+
+
+struct _param # values of training parameters
+    minIter::Int
+    maxIter::Int # max number of training iterations
+    lr::Float32
+    lr_fact::Float32 # learning rate
+    mom::Float32 # momentum term
+    thresh::Float32 # critical gradient value, break training upon falling below
+    batchSize::Int
+    testSize::Int
+    feedback::Int
+    function _param(; 
+            minIter::Int = 500,
+            maxIter::Int = 10000,
+            lr::Float32 = 1f-2,
+            lr_fact::Float32 = 1f0,            
+            mom::Float32 = 25f-2, 
+            thresh::Float32 = 1f-6,
+            batchSize::Int = -1,
+            testSize::Int = 0,
+            feedback::Int = 10)
+        return new(minIter, maxIter, lr, lr_fact, mom, thresh, batchSize, testSize, feedback)
+    end
+end
+
+
+struct _trainer
+    g_init::Array{Float32, 2}
+    g_in::Array{Float32, 3}
+    g_out::Array{Float32, 2}
+    mom_init::Array{Float32, 2}
+    mom_in::Array{Float32, 3}
+    mom_out::Array{Float32, 2}
+    upd_count_in::Array{Float32, 2}
+    upd_count_out::Array{Float32, 1}
+    update_in::BitArray{2}
+    update_out::BitArray{1}
+    function _trainer(
+            inw::InfluenceNetwork;
+            g_init::Array{Float32, 2} = Array{Float32, 2}(undef, 0, 0),
+            g_in::Array{Float32, 3} = Array{Float32, 3}(undef, 0, 0, 0),
+            g_out::Array{Float32, 2} = Array{Float32, 2}(undef, 0, 0),
+            mom_init::Array{Float32, 2} = Array{Float32, 2}(undef, 0, 0),
+            mom_in::Array{Float32, 3} = Array{Float32, 3}(undef, 0, 0, 0),
+            mom_out::Array{Float32, 2} = Array{Float32, 2}(undef, 0, 0))
+        if size(g_init)[1] != inw.n_
+            g_init = Array{Float32, 2}(undef, inw.n_, inw.m)
+        end
+        if size(g_in)[1] != inw.n_
+            g_in = Array{Float32, 3}(undef, inw.n_, inw.n_, inw.m)
+        end
+        if size(g_out)[1] != inw.n_
+            g_out = Array{Float32, 2}(undef, inw.n_, inw.m)
+        end
+        if size(mom_init)[1] != inw.n_
+            mom_init = zeros(Float32, inw.n_, inw.m)
+        end
+        if size(mom_in)[1] != inw.n_
+            mom_in = zeros(Float32, inw.n_, inw.n_, inw.m)
+        end
+        if size(mom_out)[1] != inw.n_
+            mom_out = zeros(Float32, inw.n_, inw.m)
+        end
+        upd_count_in = Array{Float32, 2}(undef, inw.n_, inw.n_)
+        upd_count_out = Array{Float32, 1}(undef, inw.n_)
+        update_in = BitArray{2}(undef, inw.n_, inw.n_)
+        update_out = BitArray{1}(undef, inw.n_)
+        return new(g_init, g_in, g_out, mom_init, mom_in, mom_out, upd_count_in, upd_count_out, update_in, update_out)
+    end
+end 
+
+
+struct _internal
+    out::Array{Float32, 1}
+    select::Array{Int, 1}
+    w_init::Array{Float32, 1} 
+    w_in::Array{Float32, 2} 
+    w_out::Array{Float32, 1}
+    s::Array{Float32, 1}
+    z::Array{Float32, 1}
+    expZ::Array{Float32, 1}
+    t_1::Array{Float32, 1}
+    t_2::Array{Float32, 1}
+    num::Array{Float32, 1}
+    dnom::Array{Float32, 1}
+    g_init::Array{Float32, 2}
+    g_in::Array{Float32, 2}
+    g_out::Array{Float32, 2}
+    g_loss::Array{Float32, 1}
+    g_init_::Array{Float32, 2}
+    g_in_::Array{Float32, 2}
+    g_loss_::Array{Float32, 2}
+    function _internal(inw::InfluenceNetwork)
+        out::Array{Float32, 1} = Array{Float32, 1}(undef, inw.m)
+        select::Array{Int, 1} = Array{Int, 1}(undef, inw.n)
+        w_init::Array{Float32, 1} = Array{Float32, 1}(undef, inw.n)
+        w_in::Array{Float32, 2} = Array{Float32, 2}(undef, inw.n, inw.n)
+        w_out::Array{Float32, 1} = Array{Float32, 1}(undef, inw.n)
+        s::Array{Float32, 1} = Array{Float32, 1}(undef, inw.n)
+        z::Array{Float32, 1} = Array{Float32, 1}(undef, inw.n)
+        expZ::Array{Float32, 1} = Array{Float32, 1}(undef, inw.n)
+        t_1::Array{Float32, 1} = Array{Float32, 1}(undef, inw.n)
+        t_2::Array{Float32, 1} = Array{Float32, 1}(undef, inw.n)
+        num::Array{Float32, 1} = Array{Float32, 1}(undef, inw.n)
+        dnom::Array{Float32, 1} = Array{Float32, 1}(undef, inw.n)
+        g_init::Array{Float32, 2} = Array{Float32, 2}(undef, inw.n, inw.m)
+        g_in::Array{Float32, 2} = Array{Float32, 2}(undef, inw.n^2, inw.m)
+        g_out::Array{Float32, 2} = Array{Float32, 2}(undef, inw.n, inw.m)
+        g_loss::Array{Float32, 1} = Array{Float32, 1}(undef, inw.m)
+        g_init_::Array{Float32, 2} = Array{Float32, 2}(undef, inw.n, inw.n)
+        g_in_::Array{Float32, 2} = Array{Float32, 2}(undef, inw.n, inw.n^2)
+        g_loss_::Array{Float32, 2} = Array{Float32, 2}(undef, inw.m, inw.m)
+        return new(out, select, w_init, w_in, w_out, s, z, expZ, t_1, t_2, num, dnom, g_init, g_in, g_out, g_loss, g_init_, g_in_, g_loss_)
+    end
+end
+
+
+struct _predictor
+    out::Array{Float32, 2}
+    select::Array{Int, 1}
+    S::Array{Float32, 2}
+    Z::Array{Float32, 2}
+    function _predictor(inw::InfluenceNetwork, data::_data)
+        out::Array{Float32, 2} = Array{Float32, 2}(undef, data.n, inw.m)
+        select::Array{Int, 1} = Array{Int, 1}(undef, inw.n)
+        S::Array{Float32, 2} = Array{Float32, 2}(undef, inw.n, inw.m)
+        Z::Array{Float32, 2} = Array{Float32, 2}(undef, inw.n, inw.m)
+        return new(out, select, S, Z)
+    end
+end
